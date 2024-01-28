913e51d939a7e589194ea7cabb3a5d94f5f5cd82
Seyacat <seyacat@gmail.com>
2024-01-27 22:17:50 -0500

    Add crypoutils genesys

--- cryptoUtils.js
+++ cryptoUtils.js
@@ -1,65 +1,122 @@
 const crypto = require("crypto");
+const { hasSubscribers } = require("diagnostics_channel");
+const JSONb = require("json-bigint");
 
 const createKeyPair = () => {
   const curve = "secp521r1";
   const { privateKey, publicKey } = crypto.generateKeyPairSync("ec", {
     namedCurve: curve,
   });
-  const publicKeyDer = publicKey
-    .export({ type: "spki", format: "der" })
+  /*const privateKeyDer = privateKey
+    .export({ type: "sec1", format: "der" })
     .toString("base64");
+  pktest = crypto.createPrivateKey({
+    key: Buffer.from(privateKeyDer, "base64"),
+    format: "der",
+    type: "sec1",
+  });
+  pkreder = pktest.export({ type: "sec1", format: "der" }).toString("base64");
+  console.log({ pktest });*/
+
+  const publicKeyDer = publicKey.export({ type: "spki", format: "der" });
+  console.log({ publicKeyDer });
+
   return {
-    privateKey: privateKey
-      .export({ type: "sec1", format: "der" })
-      .toString("base64"),
-    publicKey: publicKeyDer,
-    address:
-      "01" +
-      Buffer.from(sha256(sha256(publicKeyDer)), "hex")
-        .toString("base64")
-        .replace("/", "")
-        .replace("+", "")
-        .slice(0, 28),
+    privateKey,
+    publicKey,
+    address: createAddress(publicKey),
   };
+};
+
+const createAddress = (publicKey) => {
+  return (
+    "01" +
+    Buffer.from(
+      sha256(
+        Buffer.from(
+          sha256(publicKey.export({ type: "spki", format: "der" })),
+          "hex"
+        )
+      ),
+      "hex"
+    )
+      .toString("base64")
+      .replace("/", "")
+      .replace("+", "")
+      .slice(0, 28)
+  );
 };
 
 const sign = (data, privateKey) => {
-  const sign = crypto.createSign("SHA256");
-  sign.update(data);
-  const signature = sign.sign(privateKey);
-  return signature;
+  if (typeof data === "string") {
+    data = Buffer.from(data);
+  }
+  const ret = crypto.sign("SHA256", data, privateKey);
+  console.log({ ret });
+  return ret.toString("base64");
 };
 
-newTransaction = (address, publicKey, amount) => {
-  transaction = {
-    data: {
-      version: 1,
-      inputs: [],
-      publicKey,
-      nonce: 0,
-      outputs: [(address, amount)],
-    },
-    signature: "",
-  };
-};
-
-newGenesis = (address, publicKey) => {
-  block = {
-    address: address,
-    transactions: [newTransaction(address, publicKey, 1000000)],
+createTransaction = (address, privateKey, publicKey, amount) => {
+  const data = JSONb.stringify({
+    version: 1,
+    inputs: ["coinbase"],
+    publicKey: publicKey
+      .export({ type: "spki", format: "der" })
+      .toString("base64"),
     nonce: 0,
-    previousHash:
-      "0000000000000000000000000000000000000000000000000000000000000000",
-    hash: "0000000000000000000000000000000000000000000000000000000000000000",
+    outputs: [{ address, amount }],
+    date: Date.now(),
+  });
+  const signature = sign(data, privateKey);
+  /*console.log({ publicKey });
+  //VERIFY
+  console.log(
+    "VERIFY PRIVATE",
+    crypto.verify("SHA256", data, privateKey, Buffer.from(signature, "base64"))
+  );
+  console.log(
+    "VERIFY PUBLIC",
+    crypto.verify("SHA256", data, publicKey, Buffer.from(signature, "base64"))
+  );*/
+  const tx = {
+    data,
+    signature,
   };
+  transaction = { tx, hash: sha256(JSONb.stringify(tx)) };
+  return transaction;
+};
+
+const createGenesis = (address, privateKey, publicKey) => {
+  const initialTransaction = createTransaction(
+    address,
+    privateKey,
+    publicKey,
+    1000000
+  );
+
+  const transactionTree = {
+    root: initialTransaction.hash,
+    leafs: [initialTransaction],
+  };
+
+  const hash = sha256(JSONb.stringify(transactionTree));
+
+  block = {
+    index: 0,
+    transactions: transactionTree,
+    previousHash: null,
+    hash,
+    date: Date.now(),
+  };
+  return block;
 };
 
 const sha256 = (data) => {
   const ret = crypto.createHash("sha256").update(data).digest("hex");
-  console.log(ret);
   return ret;
 };
 
 module.exports = {
   createKeyPair,
+  createGenesis,
 };
